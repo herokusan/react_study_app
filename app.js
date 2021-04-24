defd71756c7f9698fef2f58414eb34522e99b293
taras-lytvynskyi <lytvynskyi.t@gmail.com>
2021-04-24 15:51:07 +0300

    Back-end

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// Server
+console.log("Starting App...")
+
+const express = require("express");
+const app = express();
+
+const mysql = require("mysql2");
+var migration = require('mysql-migrations');
+var connection = mysql.createConnection({
+    connectionLimit : 10,
+    host: "localhost",
+    user: "root",
+    database: "distance_learning",
+    password: "lytvynskyi"
+});
+
+
+// migration.init(connection, __dirname + '/migrations', function() {
+//   console.log("Check migrations...");
+//  });
+
+
+async function start() {
+    try {
+      await connection.connect(function(err){
+        if (err) {
+          return console.error("Error: " + err.message);
+        }
+        else{
+          console.log("MySQL connection success! :)");
+        }
+     });
+  
+      app.listen((PORT = 5000), () => {
+        console.log(`App has been stared on ${PORT}. DB is avalieble`);
+      });
+    } catch (e) {
+      console.log(`Server error ${e.message}`);
+      process.exit(1);
+    }
+  }
+  
+start();
---
+++ interface/src/components/login/login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginStyle from "./login.css"
+function Login() {
+  return (
+    <div className = "container">
+      login page!
+    </div>
+  );
+}
+
+export default Login;
